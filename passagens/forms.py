55fe27ae2319aea862fe80d3b9c9f810342d2a74
Giovani Foltran <giovanicanovaf@gmail.com>
2021-01-16 13:47:19 -0300

    first commit

--- passagens/forms.py
+++ passagens/forms.py
@@ -4,39 +4,38 @@ from datetime import datetime
 from passagens.tipo_viagem import tipos_de_classes
 
 class PassagemForms(forms.Form):
-    origem = forms.CharField(label='Origem', max_length=100)
-    destino = forms.CharField(label='Destino', max_length=100)
-    data_ida = forms.DateField(label='Ida', widget=DatePicker())
-    data_volta = forms.DateField(label='Volta', widget=DatePicker())
-    data_pesquisa = forms.DateField(label='Data da pesquisa', disabled=True, initial=datetime.today)
-    classe_viagem = forms.ChoiceField(label='Classe do vôo', choices=tipos_de_classes)
-    informacoes = forms.CharField(
-        label='Informacoes extras',
+    nome_proprietário = forms.CharField(label='Nome do Proprietário', max_length=100)
+    cpf_proprietario = forms.CharField(label='CPF do proprietário', max_length=15)
+    rua = forms.CharField(label='Rua', max_length=100)
+    bairro = forms.CharField(label='Bairro', max_length=100)
+    numero = forms.IntegerField(label='Número')
+    entrada = forms.CharField(label='Descreva a entrada', max_length=500, required=False)
+    sala = forms.CharField(label='Descreva a sala', max_length=500, required=False)
+    copa = forms.CharField(label='Descreva a copa', max_length=500, required=False)
+    cozinha = forms.CharField(label='Descreva a cozinha', max_length=500, required=False)
+    corredor_interno = forms.CharField(label='Descreva o corredor interno', max_length=500, required=False)
+    wc_social = forms.CharField(label='Descreva o WC social', max_length=500, required=False)
+    dormitorio_um = forms.CharField(label='Descreva o primeiro dormitório', max_length=500, required=False)
+    dormitorio_dois = forms.CharField(label='Descreva o segundo dormitório', max_length=500,required=False)
+    dormitorio_tres = forms.CharField(label='Descreva o terceiro dormitório', max_length=500, required=False)
+    dormitorio_quatro = forms.CharField(label='Descreva o quarto dormitório', max_length=500, required=False)
+    suite = forms.CharField(label='Descreva a suíte', max_length=500, required=False)
+    wc_suite = forms.CharField(label='Descreva o WC da suíte', max_length=500, required=False)
+    corredor_externo = forms.CharField(label='Descreva o corredor externo', max_length=500, required=False)
+    piso_superior = forms.CharField(label='Descreva o piso superior', max_length=500, required=False)
+    observacoes = forms.CharField(
+        label='Observações',
         max_length=200,
         widget=forms.Textarea(),
         required=False
     )
-    email = forms.EmailField(label='Email', max_length=150)
-
-    def clean_origem(self):
-        origem = self.cleaned_data.get('origem')
-        if any(char.isdigit() for char in origem):
-            raise forms.ValidationError('Origem inválida, não inclua números ou caracteres especiais.')
-        else:
-            return origem
+    nome_locatario = forms.CharField(label='Nome do locatário', max_length=100)
+    cpf_locatario = forms.CharField(label='CPF do locatário', max_length=15)
+    nome_fiador = forms.CharField(label='Nome do fiador', max_length=100)
+    cpf_fiador = forms.CharField(label='CPF do fiador', max_length=15)
+    
 
 
-    def clean_destino(self):
-        destino = self.cleaned_data.get('destino')
-        if any(char.isdigit() for char in destino):
-            raise forms.ValidationError('Destino inválido, não inclua números ou caracteres especiais.')
-        else:
-            return destino
+    
 
-
-    def clean(self):
-        origem = self.cleaned_data.get('origem')
-        destino = self.cleaned_data.get('destino')
-        if origem == destino:
-            self.add_error('destino', 'Origem e destino não podem ser iguais')
-        return self.cleaned_data
+    
